76b596f64cce11c9b270838dfd01e8f7c7a0ab5d
lurgi <wahoo9040@naver.com>
2023-10-29 13:30:20 +0900

    feat : #2
    feat : #3
    
    fix (#3): fix

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 //feat 1.
 confirm.log("first feat");
+
+//feat 2.
+confirm.log("second feat");
+
+//feat 3.
+confirm.log("third feat");
+
+console.log("hi");
+
+console.log("hi");
+
+console.log("hi");
